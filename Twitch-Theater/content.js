741381b8b236d9247bb85ed68804301722792860
MuraYuki <40485102+murayuki@users.noreply.github.com>
2022-11-23 18:33:34 +0800

    Add files via upload

--- Twitch-Theater/content.js
+++ Twitch-Theater/content.js
@@ -3,6 +3,7 @@ const excludeUrl = [,
     /.*:\/\/.*\.twitch\.tv\/settings\/.*/,
 ]
 let loadingFailed = 120
+
 window.addEventListener("load", () => {
     console.log("Plugin loaded ....")
     if (excludeUrl.some(url => window.location.href.match(url)))
@@ -28,31 +29,6 @@ setInterval(() => {
     if (excludeUrl.some(url => window.location.href.match(url)))
         return
 
-    const loading = document.querySelector('.tw-loading-spinner')
-    if (!loading) {
-        loadingFailed = 120
-        return
-    }else {
-        loadingFailed--
-        console.log(`Player Loading Failed | Auto Refresh Second ${loadingFailed}`)
-        if (loadingFailed >= 1)
-            return
-        
-        location.reload()
-        loadingFailed = 120
-    }
-
-    const errorBtn = document.querySelector('.ScCoreButtonDestructive-sc-ocjdkq-4')
-    if (!loading) 
-        return
-    errorBtn.click()
-}, 1000);
-
-
-setInterval(() => {
-    if (excludeUrl.some(url => window.location.href.match(url)))
-        return
-
     const errorBtn = document.querySelector('.ScCoreButtonDestructive-sc-ocjdkq-4')
     if (!errorBtn) 
         return
@@ -61,7 +37,31 @@ setInterval(() => {
         console.log("Error Click Button")
         errorBtn.click()
     }, 500)
-}, 2000);
+}, 5000);
+
+setInterval(() => {
+    if (excludeUrl.some(url => window.location.href.match(url)))
+        return
+
+    const loading = document.querySelector('.tw-loading-spinner')
+    if (!loading) {
+		if (loadingFailed == 120)
+			return
+		
+		loadingFailed = 120
+		console.log(`Player Loading Failed | Auto Refresh Second "Reset" ${loadingFailed}`)
+        return
+    }else {
+        loadingFailed--
+        console.log(`Player Loading Failed | Auto Refresh Second ${loadingFailed}`)
+        
+		
+		if (loadingFailed >= 1)
+            return
+        location.reload()
+        loadingFailed = 120
+    }
+}, 1000);
 
 function toggleTheaterMode() {
     setTimeout(() => {
@@ -73,7 +73,6 @@ function toggleTheaterMode() {
             return
         chatBtn.click()
     }, 500);
-
 
     setTimeout(() => {
         const theaterMode = document.querySelector(`[data-a-target="player-theatre-mode-button"]`)
